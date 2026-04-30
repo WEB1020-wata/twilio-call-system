f068cf8bd937a2d0ab978447d4c0977e029e3b9a
WEB1020-wata <1020watanabe01@gmail.com>
2026-04-30 21:07:47 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,21 +3,21 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/voice", (req, res) => {
-  const twiml = `<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-  <Say language="ja-JP">Test success.</Say>
-</Response>`;
-
-  res.type("text/xml; charset=utf-8");
-  res.send(twiml);
-});
-
 app.get("/", (req, res) => {
   res.send("server is running");
 });
 
+app.all("/voice", (req, res) => {
+  const twiml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Say language="ja-JP">お電話ありがとうございます。テストは成功です。</Say>
+</Response>`;
+
+  res.set("Content-Type", "text/xml; charset=utf-8");
+  res.send(twiml);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log("Server started");
+  console.log(`Server started on port ${port}`);
 });
