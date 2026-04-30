88612188af168aa45a11ddeb82cb7d72a6912354
WEB1020-wata <1020watanabe01@gmail.com>
2026-04-30 20:11:42 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("サーバー動いてます！");
+});
+
+app.post("/voice", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(3000, () => {
+  console.log("Server started");
+});
