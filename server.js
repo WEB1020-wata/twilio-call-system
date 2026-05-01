6f47f8901c46c9751fbe573327829596305c82a7
WEB1020-wata <1020watanabe01@gmail.com>
2026-05-01 19:09:47 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,45 +8,10 @@ app.get("/", (req, res) => {
   res.send("server is running");
 });
 
-// 最初の応答：話してもらう
 app.all("/voice", (req, res) => {
   const twiml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Gather input="speech" language="ja-JP" action="/gather" method="POST" timeout="5" speechTimeout="auto">
-    <Say language="ja-JP">お電話ありがとうございます。ご用件をお話しください。</Say>
-  </Gather>
-  <Say language="ja-JP">申し訳ございません。聞き取れませんでした。もう一度お願いいたします。</Say>
-  <Redirect method="POST">/voice</Redirect>
-</Response>`;
-
-  res.set("Content-Type", "text/xml; charset=utf-8");
-  res.send(twiml);
-});
-
-// 聞き取った内容に反応
-app.post("/gather", (req, res) => {
-  const speechResult = req.body.SpeechResult || "";
-
-  if (!speechResult) {
-    const twiml = `<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-  <Say language="ja-JP">申し訳ございません。聞き取れませんでした。もう一度お願いいたします。</Say>
-  <Redirect method="POST">/voice</Redirect>
-</Response>`;
-
-    res.set("Content-Type", "text/xml; charset=utf-8");
-    return res.send(twiml);
-  }
-
-  const safeText = speechResult
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;");
-
-  const twiml = `<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-  <Say language="ja-JP">${safeText}、ですね。</Say>
-  <Say language="ja-JP">続けて詳しい内容をお話しください。録音を開始します。</Say>
+  <Say language="ja-JP">お電話ありがとうございます。ご用件を録音いたします。ピーという音のあとにお話しください。</Say>
   <Record
     action="/recording-finished"
     method="POST"
@@ -62,7 +27,6 @@ app.post("/gather", (req, res) => {
   res.send(twiml);
 });
 
-// 録音完了後
 app.post("/recording-finished", (req, res) => {
   console.log("録音完了:", req.body);
 
